ee705c40cf961c65727d698b9a4b7ee2d564e3a7
cuter44 <cuter44@foxmail.com>
2012-09-07 00:48:43 +0800

    ini 读取机能完成
    已完成 ini 读取机能的编写及模块测试, 该组四个方法目前可正确运作
    
    Signed-off-by: cuter44 <cuter44@foxmail.com>

--- JUkaUtility.java
+++ JUkaUtility.java
@@ -48,19 +48,17 @@ public class JUkaUtility
     /**
      * <p>从指定的 ini 文件中定位指定的节</p>
      * <p>主要是为了快速查找位置供其他 API 或第三方读取用的</p>
-     * @return 已经指向指定位置的文件流, 如果指定文件或节不存在则返回 null.
+     * @return 指向请求节的下一行行首的 Scanner, 如果指定文件或节不存在则返回 null.
      */
-    public static FileInputStream iniLocate(String argFileName, String argSector)
+    public static Scanner iniLocateSector(String argFileName, String argSector)
     {
         File iniFile = null;
-        FileInputStream iniStream = null;
         Scanner iniScanner = null;
 
         // 试图读取文件
         try
         {
             iniFile = new File(argFileName);
-            iniStream = new FileInputStream(iniFile);
             iniScanner = new Scanner(new FileInputStream(iniFile));
         }
         catch (FileNotFoundException ex)
@@ -79,10 +77,7 @@ public class JUkaUtility
         {
             String buffer = iniScanner.nextLine();
             if (buffer.matches("\\s*\\[" + argSector + "\\]"))
-            {
-                //iniScanner.close();
-                return(iniStream);
-            }
+                return(iniScanner);
         }
 
         iniScanner.close();
@@ -91,27 +86,28 @@ public class JUkaUtility
 
     /**
      * <p>从当前位置开始枚举解析并返回键-值对, 遇到节结束时终止</p>
-     * <p>纯粹是因为 iniRead(Sector型) 返回的数据是无序的所以才写的这个.</p>
+     * <p>纯粹是因为 iniReadSector() 返回的数据是无序的所以才写的这个.<br>
+     * (*) 每次调用该函数都将使 Scanner 的读取位置后移<br>
+     * (*) 当函数返回 null 即读取节完成时, 将关闭作为参数的 Scanner</p>
      * @return 返回一个二元 String 数组, String[0] 表示键, String[1] 表示值, 如果已枚举到段末尾则返回 null.
      */
-    public static String[] iniEnumSector(FileInputStream iniStream)
+    public static String[] iniEnumSector(Scanner argIniScanner)
     {
-        Scanner iniScanner = new Scanner(iniStream);
         String[] rtString = null;
         String buffer = null;
 
         // 文件末尾时返回 null
-        while (iniScanner.hasNextLine())
+        while (argIniScanner.hasNextLine())
         {
-            buffer = iniScanner.nextLine();
+            buffer = argIniScanner.nextLine();
 
-            // 跳过注释行
+            // 显式跳过注释行
             if (buffer.matches("\\s;"))
                 continue;
 
             // 节末尾时返回 null
             if (buffer.matches("\\s*\\[.*\\]"))
-                return(null);
+                break;
 
             // 以 = 为基准匹配键-值对
             // 键首尾去空格, 值\n转换为回车符
@@ -120,11 +116,14 @@ public class JUkaUtility
                 rtString = buffer.split("=",2);
                 rtString[0] = rtString[0].trim();
                 rtString[1] = rtString[1].replaceAll("\\n","\n");
-                iniScanner.close();
                 return(rtString);
             }
+
+            // 警告无法识别行, 但不打断执行
+            System.err.println("略过无法识别的ini内容: " + buffer);
         }
 
+        argIniScanner.close();
         return(null);
     }
 
@@ -133,61 +132,47 @@ public class JUkaUtility
      * @return 请求读取的节的哈希表, 如果指定文件或节不存在则返回 null,
      * 如果指定节为空则返回一个空的哈希表.
      */
-    public static Hashtable<String, String> iniRead(String argFileName, String argSector)
+    public static Hashtable<String, String> iniReadSector(String argFileName, String argSector)
     {
-        FileInputStream iniStream = JUkaUtility.iniLocate(argFileName, argSector);
+        Scanner iniScanner = JUkaUtility.iniLocateSector(argFileName, argSector);
 
         // 参数不正确时返回 null.
-        if (iniStream == null)
+        if (iniScanner == null)
             return(null);
 
         Hashtable<String, String> rtHtSector = new Hashtable<String, String>();
         String[] buffer = null;
 
-        while ((buffer=iniEnumSector(iniStream)) != null)
+        // 迭代读取
+        while ((buffer=iniEnumSector(iniScanner)) != null)
             rtHtSector.put(buffer[0],buffer[1]);
 
-        try
-        {
-            iniStream.close();
-        }
-        catch (IOException ex)
-        {
-            // This exception merely occures
-            System.err.println(ex);
-        }
         return(rtHtSector);
     }
 
     /**
      * <p>从指定的 ini 文件中读取指定节和键的值</p>
      * <p>简单地说就是:<br>
-     * <p>[argSector]<br>
+     * [argSector]<br>
      * argKey=Value</p>
      * 总之, 返回的是 Value 就对了.</p>
+     * @return 一个字符串表示所请求的值, 如果没有这样的值则返回 null;
      */
     public static String iniRead(String argFileName, String argSector, String argKey)
     {
-        FileInputStream iniStream = JUkaUtility.iniLocate(argFileName, argSector);
+        Scanner iniScanner = JUkaUtility.iniLocateSector(argFileName, argSector);
 
         // 参数不正确时返回 null.
-        if (iniStream == null)
+        if (iniScanner == null)
             return(null);
 
         String[] buffer = null;
 
-        while ((buffer=iniEnumSector(iniStream)) != null)
+        // 迭代读取并判断
+        while ((buffer=iniEnumSector(iniScanner)) != null)
             if (buffer[0].equals(argKey))
             {
-                try
-                {
-                    iniStream.close();
-                }
-                catch (IOException ex)
-                {
-                    // This exception merely occured.
-                    System.err.println(ex);
-                }
+                iniScanner.close();
                 return(buffer[1]);
             }
 
@@ -207,8 +192,18 @@ public class JUkaUtility
         System.out.println(JUkaUtility.getProgramPath());
 
         // 此代码段用于测试 读取ini
-        JUkaUtility.iniLocate(JUkaUtility.getProgramPath() + "shell/SampleShell/SampleShell.ini", "images");
-        ht = iniRead(JUkaUtility.getProgramPath() + "shell/SampleShell/SampleShell.ini", "images");
+        //JUkaUtility.iniLocateSector(JUkaUtility.getProgramPath() + "shell/SampleShell/SampleShell.ini", "images");
+        ht = JUkaUtility.iniReadSector(
+            JUkaUtility.getProgramPath() + "shell/SampleShell/SampleShell.ini",
+            "images"
+        );
+        System.out.println(
+            JUkaUtility.iniRead(
+                JUkaUtility.getProgramPath() + "shell/SampleShell/SampleShell.ini",
+                "images",
+                "0000"
+            )
+        );
 
         return;
     }
